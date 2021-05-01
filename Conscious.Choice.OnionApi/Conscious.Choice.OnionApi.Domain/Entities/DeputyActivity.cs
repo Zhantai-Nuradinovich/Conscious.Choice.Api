7bd5a975e7b835720a27eaeae1abc4e3019561ea
Zhantai Nuradinov <jantai.nuradinovich@gmail.com>
2021-05-02 00:51:26 +0600

    Added DeputyActivityModel

---
+++ Conscious.Choice.OnionApi/Conscious.Choice.OnionApi.Domain/Entities/DeputyActivity.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Conscious.Choice.OnionApi.Domain.Entities
+{
+    public class DeputyActivity
+    {
+        public string Name { get; set; }
+        public List<Vote> Votes { get; set; }
+    }
+}
---
+++ Conscious.Choice.OnionApi/Conscious.Choice.OnionApi.Service/Features/DeputyFeatures/Queries/GetDeputyActivityQuery.cs
@@ -0,0 +1,35 @@
+﻿using Conscious.Choice.OnionApi.Domain.Entities;
+using Conscious.Choice.OnionApi.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Conscious.Choice.OnionApi.Service.Features.DeputyFeatures.Queries
+{
+    public class GetDeputyActivityQuery : IRequest<DeputyActivity>
+    {
+        public string Name { get; set; }
+        public class GetDeputyActivityQueryHandler : IRequestHandler<GetDeputyActivityQuery, DeputyActivity>
+        {
+            private readonly IApplicationDbContext _context;
+            public GetDeputyActivityQueryHandler(IApplicationDbContext context)
+            {
+                _context = context;
+            }
+            public async Task<DeputyActivity> Handle(GetDeputyActivityQuery request, CancellationToken cancellationToken)
+            {
+                DeputyActivity deputy = new DeputyActivity();
+                deputy.Name = request.Name;
+                int id = _context.Deputies.Where(d => d.Name == request.Name).FirstOrDefault().Id;
+                var Votes = _context.Votes.Include(v => v.Law).Where(v => v.DeputyId == id).ToList();
+
+                deputy.Votes = Votes;
+                if (deputy == null) return null;
+                return deputy;
+            }
+        }
+    }
+}
